0d7165198e9ef608720d77912438f77b80029ae0
JessePinkman <bum8hj123@gmail.com>
2026-02-06 17:55:19 -0500

    Setup ESLint and Prettier

--- CollectorsArchive/collectorsarchive.client/src/App.tsx
+++ CollectorsArchive/collectorsarchive.client/src/App.tsx
@@ -1,24 +1,23 @@
-import { createTheme, MantineProvider, MantineThemeProvider, type CSSVariablesResolver } from '@mantine/core';
-import '@mantine/core/styles.css';
-import './App.css';
+import { createTheme, MantineProvider, MantineThemeProvider, type CSSVariablesResolver } from "@mantine/core"
+import "@mantine/core/styles.css"
+import "./App.css"
 
-const theme = createTheme({
-});
+const theme = createTheme({})
 
-const resolver: CSSVariablesResolver = (theme) => ({
+const resolver: CSSVariablesResolver = () => ({
 	variables: {},
 	light: {},
 	dark: {},
 })
 
 function App() {
-  return (
+	return (
 		<MantineThemeProvider theme={theme}>
-      <MantineProvider theme={theme} defaultColorScheme="auto" cssVariablesResolver={resolver}>
-        Collector's Archive
-      </MantineProvider>
-    </MantineThemeProvider>
-  )
+			<MantineProvider theme={theme} defaultColorScheme="auto" cssVariablesResolver={resolver}>
+				Collector's Archive
+			</MantineProvider>
+		</MantineThemeProvider>
+	)
 }
 
 export default App
