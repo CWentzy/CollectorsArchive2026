98123b8c49f03c52142951dc7fe373d9bd143649
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2026-02-16 21:13:22 -0500

    Install EF Core 8, SQL Server provider, and EF Tools; added DataDb folder and controllers for User Registration & Login backend work

---
+++ CollectorsArchive/CollectorsArchive.Server/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectorsArchive.Server
+{
+    // Models/User.cs this file is for the user model, which will be used to store user information in the database. It will also be used to authenticate users when they log in. The password field will be used to store the user's password, but for google authentication,
+    // I will store the sub (subject) field from the google token in this field instead of a password.
+    public class UserInformation
+    {
+        public int DatabaseId { get; set; }// database id
+        public string Email { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+
+}
