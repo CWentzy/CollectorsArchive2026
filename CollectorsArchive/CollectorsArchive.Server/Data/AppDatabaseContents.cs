522376f1eb8eb5ff9f297feffe1c9e68bdd745f3
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2026-02-16 21:46:12 -0500

    Add UserInformation model and AppDatabaseContents DbContext setup

---
+++ CollectorsArchive/CollectorsArchive.Server/Data/AppDatabaseContents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using CollectorsArchive.Server.Models;
+
+
+namespace CollectorsArchive.Server
+{
+    public class AppDatabaseContents: DbContext
+    {
+        public AppDatabaseContents(DbContextOptions<AppDatabaseContents> options) 
+            : base(options) { }
+
+        // this represets the table in the database that will store user information.
+        // The name of the table will be "Users" and it will have the columns UserId, Email, UserName
+        public DbSet<UserInformation> UserInformation { get; set; }
+    }
+}
--- CollectorsArchive/CollectorsArchive.Server/Models/UserInformation.cs
+++ CollectorsArchive/CollectorsArchive.Server/Models/UserInformation.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CollectorsArchive.Server
+namespace CollectorsArchive.Server.Models
 {
     // Models/User.cs this file is for the user model, which will be used to store user information in the database. It will also be used to authenticate users when they log in. The password field will be used to store the user's password, but for google authentication,
     // I will store the sub (subject) field from the google token in this field instead of a password.
     public class UserInformation
     {
-        public int DatabaseId { get; set; }// database id
+        public int UserId { get; set; }// database id
         public string Email { get; set; } = string.Empty;
         public string UserName { get; set; } = string.Empty;
     }
