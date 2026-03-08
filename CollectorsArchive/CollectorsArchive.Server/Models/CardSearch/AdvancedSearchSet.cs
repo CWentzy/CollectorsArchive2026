9884e939888f2706153025413985b3f304c4ce48
CWentzy <cwentzlaff4749@constogac.on.ca>
2026-03-08 07:58:42 -0400

    Added first version of the input models used to search the database.

---
+++ CollectorsArchive/CollectorsArchive.Server/Models/CardSearch/AdvancedSearchSet.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * PROGRAMMER:      Curtis Wentzlaff (7274749)
+ * FILENAME:        AdvancedSearchSet.cs
+ * ASSIGNMENT:      PROG3221 - Capstone
+ * DESCRIPTION:     Models the serialized information from the user input for Card Set searching.
+ *                  Used for displaying all cards printed within a specified Set.
+ */
+
+namespace CollectorsArchive.Server.Models.CardSearch
+{
+    public class AdvancedSearchSet
+    {
+        public int GameID { get; set; }
+        public string? Name { get; set; }
+    }
+}
