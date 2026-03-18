65592581b317155a06c4a63f8d462ba37cb69602
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2026-03-18 16:53:52 -0400

    Fixed the CardDisplayYGO model with stored procedure columns

--- CollectorsArchive/CollectorsArchive.Server/Models/CardDisplays/CardDisplayYGO.cs
+++ CollectorsArchive/CollectorsArchive.Server/Models/CardDisplays/CardDisplayYGO.cs
@@ -11,14 +11,23 @@ namespace CollectorsArchive.Server.Models.CardDisplays
     public class CardDisplayYGO
     {
         public string CardID { get; set; } = string.Empty;
-        public string name { get; set; } = string.Empty ;
-        public string superType { get; set; } = string .Empty ;
+        public string name { get; set; } = string.Empty;
+        public string superType { get; set; } = string.Empty;
         public string subType { get; set; } = string.Empty;
         public string cardText { get; set; } = string.Empty;
+
         public string? attribute { get; set; }
+
+        // Stored procedure does NOT return classifications
         public string[]? classifications { get; set; }
+
         public int? level { get; set; }
         public int? Atk { get; set; }
         public int? Def { get; set; }
+
+        // this will  matches SQL procedure  output
+        public int? PendulumScale { get; set; }
+        public int? LinkRating { get; set; }
     }
 }
+
