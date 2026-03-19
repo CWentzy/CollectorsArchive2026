4d79086e837c036b31e6d3f79ba8520437c250e3
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2026-03-19 11:44:23 -0400

    changed few things in controller that register non google user into the db

--- CollectorsArchive/CollectorsArchive.Server/Models/NonGoogleUserRequestModel.cs
+++ CollectorsArchive/CollectorsArchive.Server/Models/NonGoogleUserRequestModel.cs
@@ -2,7 +2,9 @@
 {
     public class NonGoogleUserRequestModel
     {
-        public string Email { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }   
     }
+
 }
