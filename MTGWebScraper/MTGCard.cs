474196e54040df735205a7d5219a865bd6923c9e
CWentzy <cwentzlaff4749@constogac.on.ca>
2026-03-28 06:07:30 -0400

    Added stored procedures and tables for storing Magic the Gathering data by web scraping the official website database.

---
+++ MTGWebScraper/MTGCard.cs
@@ -0,0 +1,16 @@
+﻿namespace MTGWebScraper
+{
+    internal class MTGCard
+    {
+        public string CardID { get; set; }
+        public string ManaCost { get; set; }
+        public string CardName { get; set; }
+        public string CardText { get; set; }
+        public string SuperType { get; set; }
+        public string Type { get; set; }
+        public string SubType { get; set; }
+        public string Power { get; set; }
+        public string Toughness { get; set; }
+        public string Loyalty { get; set; }
+    }
+}
---
+++ MTGWebScraper/MTGSet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MTGWebScraper
+{
+    internal class MTGSet
+    {
+        public string UrlExtension { get; set; }
+        public int SetID { get; set; }
+        public string SetName { get; set; }
+        public string SetCode { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
