684d09cd625ac22d65a9efdbbcb9ae576cfb91c5
JessePinkman <bum8hj123@gmail.com>
2026-04-04 02:52:35 -0400

    Fixes and improvements to profile panel and profile & members pages. Minor redesign to avatars
    Noteworthy changes:
    - when opening a user's profile page directly from url (i.e. not when navigating from members page) it will now fetch the user info instead of showing nothing
    - fixed profile panel infinite fetch loop
    - added profile redirect when clicking avatar in profile panel

--- CollectorsArchive/collectorsarchive.client/src/utils.ts
+++ CollectorsArchive/collectorsarchive.client/src/utils.ts
@@ -10,3 +10,8 @@ export function getCardImageUrl(gameID: string, cardID: string): string {
 	const unpaddedCardID = cardID.replace(/^0+/, "") // Remove zeroes from the start of the cardID
 	return `${import.meta.env.VITE_CARD_IMAGE_URL_YGO}/${unpaddedCardID}.jpg`
 }
+
+export const formatDate = (dateStr: string | null) => {
+	if (!dateStr) return "Unknown"
+	return new Date(dateStr).toLocaleDateString("en-US", { month: "short", year: "numeric" })
+}
